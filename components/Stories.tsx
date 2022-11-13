92dbfceda2d0b133c4ae7b60da04ff9c60965b99
goofynugtz <rahulranjan25.RR@gmail.com>
2022-11-13 16:05:29 +0530

    Fixed Stories thumbnails

--- components/Stories.tsx
+++ components/Stories.tsx
@@ -6,13 +6,13 @@ const Stories = () => {
       <h1>Latest Stories</h1>
       <div className="story-card">
         <div className="marble marble-1">
-          <Image alt='' src='/assets/DSC_4629.jpg' width={128} height={128}/>
+          <Image alt='' src='/assets/DSC_4629.JPG' width={128} height={128}/>
         </div>
         <div className="marble marble-2">
-          <Image alt='' src='/assets/DSC_4687.jpg' width={128} height={128}/>
+          <Image alt='' src='/assets/DSC_4687.JPG' width={128} height={128}/>
         </div>
         <div className="marble marble-3">
-          <Image alt='' src='/assets/DSC_4657.jpg' width={128} height={128} />
+          <Image alt='' src='/assets/DSC_4657.JPG' width={128} height={128} />
         </div>
       </div>
     </div>
