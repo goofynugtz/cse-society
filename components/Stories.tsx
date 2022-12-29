64fcdb781c2ffa2e8a98b79750cbf88fa003ebc7
goofynugtz <rahulranjan25.RR@gmail.com>
2022-12-29 22:30:45 +0530

    Fixed blog grid

--- components/Stories.tsx
+++ components/Stories.tsx
@@ -3,24 +3,47 @@ import Link from 'next/link';
 
 const Stories = ({ stories }: any) => {
   return (
-    
     <div className="stories">
       <Link href="/stories">
-      <h1>Latest Stories</h1>
+        <h1>Latest Stories</h1>
       </Link>
       <div className="story-cards">
         {stories.map((i: any) => {
           return (
             <Link key={i.name} href={`/stories/${i.name}`}>
-            <div key={i.name} className="story">
-              <div className="marble" >
-                <Image alt='' src={`/assets/${i.thumbnail}`} fill />
+              <div key={i.name} className="story">
+                <div className="marble" >
+                  <Image alt='' src={`/assets/${i.thumbnail}`} fill />
+                </div>
+                <div className="label">{`${i.name}`}</div>
               </div>
-              <div className="label">{`${i.name}`}</div>
-            </div>
             </Link>
           )
         })}
+        <Link href={`/stories/`}>
+          <div className="story">
+            <div className="marble" >
+              <div></div>
+            </div>
+            <div className="label">Placeholder A</div>
+          </div>
+        </Link>
+        <Link href={`/stories/`}>
+          <div className="story">
+            <div className="marble" >
+              <div></div>
+            </div>
+            <div className="label">Placeholder B</div>
+          </div>
+        </Link>
+        <Link href={`/stories/`}>
+          <div className="story">
+            <div className="marble" >
+              <div></div>
+            </div>
+            <div className="label">Placeholder C</div>
+          </div>
+        </Link>
       </div>
     </div>
   )
